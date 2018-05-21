9a9656c0ea48c74311a560d0f9d89258b73c32c3
Avi Verma <sungodavi@gmail.com>
2018-05-20 21:27:15 -0500

    Added setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+from cx_Freeze import setup, Executable
+
+files = ['newspapers.json', 'style.json']
+packages = ["os", "spacy"]
+excludes = ["tkinter"]
+modules = ["numpy.core._methods", "numpy.lib.format", "cymem", "murmurhash"]
+
+build_exe_options = {"packages": packages,
+                     "excludes": excludes,
+                     "includes": modules,
+                     "include_files": files}
+
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name="Sentence",
+      version="0.1",
+      description="Sentence Worksheet Maker",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("main.py", base=base)])
