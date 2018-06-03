49170d11f996c52b335aa94d5a9023a1e3319779
Avi Verma <sungodavi@gmail.com>
2018-06-03 18:10:05 -0500

    Removed virtual env

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 import sys
 from cx_Freeze import setup, Executable
 
-files = ['newspapers.json', 
-         'style.json']
-packages = ["os", 
-            "spacy.lang.en", 
+PYTHON_INSTALLATION = 'C:\\Users\\sungo\\Anaconda3'
+files = ['config',
+         'images',
+         '{}\\Lib\\site-packages\\google_cloud_firestore-0.28.0.dist-info'.format(PYTHON_INSTALLATION),
+         '{}\\Lib\\site-packages\\google_gax-0.15.16.dist-info'.format(PYTHON_INSTALLATION),
+         '{}\\Lib\\site-packages\\google_api_core-0.1.4.dist-info'.format(PYTHON_INSTALLATION),
+         '{}\\Lib\\site-packages\\google_cloud_core-0.28.1.dist-info'.format(PYTHON_INSTALLATION),
+         '{}\\Lib\\site-packages\\grpcio-1.12.0.dist-info'.format(PYTHON_INSTALLATION)]
+packages = ["os",
+            "spacy.lang.en",
             "pkg_resources._vendor",
             "idna",
             "google",
-            "google.cloud"]
-
+            "google.auth"]
 excludes = ["tkinter"]
-modules = ["numpy.core._methods", 
-           "numpy.lib.format", 
-           "cymem", 
+modules = ["numpy.core._methods",
+           "numpy.lib.format",
+           "cymem",
            "murmurhash",
            "lxml._elementpath",
            "cytoolz._signatures",
-           "thinc.neural._classes.difference"]
+           "thinc.neural._classes.difference",
+           "spacy.tokens.underscore",
+           "spacy.tokens.printers"]
 
 build_exe_options = {"packages": packages,
                      "excludes": excludes,
@@ -35,4 +42,4 @@ setup(name="Sentence",
       version="0.1",
       description="Sentence Worksheet Maker",
       options={"build_exe": build_exe_options},
-      executables=[Executable("main.py", base=base)])
+      executables=[Executable("main.py", base=base, icon='doc_icon.ico')])
